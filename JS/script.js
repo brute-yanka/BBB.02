d04e0da0123cd0ee436aa8650b1c9f29ce00470e
Peter Onadi <onadipeter.nyirsuli@gmail.com>
2023-10-12 21:28:15 +0200

    Removed test

--- JS/script.js
+++ JS/script.js
@@ -113,8 +113,6 @@ function createToast(type, icon, text) {
     setTimeout(() => toast.remove(), 5000);
 };
 
-initGame();
-roundsValue = 2;
 //========== INITIALIZE ==========
 function initGame() {
     createToast('info', '<i class="ri-information-line"></i>', 'A játékot a Fehér játékos tudja kezdeni!');
